48eb70beb86acb632cab28b41a0b515ca885ee18
cuteiswhatweaim4 <peteracaylar02@gmail.com>
2024-10-28 12:50:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ function showContent(topic) {
     sections.forEach(section => {
         section.style.display = 'none'; // Hide all sections
     });
-    document.getElementById(topic).style.display = 'block'; // Show selected section
-}
-
-function showHome() {
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => {
-        section.style.display = 'none'; // Hide all sections
-    });
+    
+    if (topic === 'home') {
+        document.querySelector('.main-links').style.display = 'block'; // Show main links
+    } else {
+        document.querySelector('.main-links').style.display = 'none'; // Hide main links
+        document.getElementById(topic).style.display = 'block'; // Show selected section
+    }
 }
